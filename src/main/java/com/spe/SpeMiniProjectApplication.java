a2b88b3413b0bbf0bd7d4855532fb9478359492e
jaypanchal14 <jay1402panchal@gmail.com>
2024-02-21 16:31:09 +0530

    new branch

--- src/main/java/com/spe/SpeMiniProjectApplication.java
+++ src/main/java/com/spe/SpeMiniProjectApplication.java
@@ -10,72 +10,7 @@ public class SpeMiniProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpeMiniProjectApplication.class, args);
-        System.out.println("Available operations in our calculator:");
-        System.out.println("1)Addition");
-        System.out.println("2)Subtraction");
-        System.out.println("3)Multiplication");
-        System.out.println("4)Division");
-        System.out.println("5)Square-root");
-        System.out.println("6)Modulo");
-        System.out.println("Please select an operation from above (between 1 to 6): ");
+        System.out.println("Build successfully with jenkins");
 
-        Scanner s = new Scanner(System.in);
-
-        int operation = s.nextInt();
-        double num1, num2;
-
-        switch(operation){
-            case 1:
-                System.out.println("Enter two numbers separated by space(s): ");
-                num1 = s.nextDouble();
-                num2 = s.nextDouble();
-                System.out.println("Result of addition: "+(num1+num2));
-                break;
-            case 2:
-                System.out.println("Enter two numbers separated by space(s): ");
-
-                num1 = s.nextDouble();
-                num2 = s.nextDouble();
-                System.out.println("Result of subtraction: "+(num1-num2));
-                break;
-            case 3:
-                System.out.println("Enter two numbers separated by space(s): ");
-
-                num1 = s.nextDouble();
-                num2 = s.nextDouble();
-                System.out.println("Result of multiplication: "+(num1*num2));
-                break;
-            case 4:
-                System.out.println("Enter two numbers separated by space(s): ");
-
-                num1 = s.nextDouble();
-                num2 = s.nextDouble();
-                if(num2 == 0){
-                    System.out.println("Division by zero is not allowed");
-                }else{
-                    System.out.println("Result of division: "+(num1/num2));
-                }
-                break;
-            case 5:
-                System.out.println("Enter a number:");
-                num1 = s.nextDouble();
-                System.out.println("Result of square-root: "+Math.sqrt(num1));
-                break;
-            case 6:
-                System.out.println("Enter two numbers separated by space(s): ");
-
-                num1 = s.nextDouble();
-                num2 = s.nextDouble();
-                if(num2 == 0){
-                    System.out.println("Division by zero is not allowed.");
-                }else {
-                    System.out.println("Result of Modulo: " + (num1 % num2));
-                }
-                break;
-            default:
-                System.out.println("Invalid input given while selecting operation. Exiting...");
-                //break;
-        }
-        s.close();
     }
 }
